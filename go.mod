61736aff4e6254c2cb5b1d49d83e19d171af59bf
Nanohard <code@nanohard.net>
2019-05-31 23:19:27 -0500

    chore(core): update binary to 1.0.7

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.12
 
 require (
 	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25
-	github.com/kelindar/binary v1.0.4
+	github.com/kelindar/binary v1.0.7
 )
--- scribble.go
+++ scribble.go
@@ -15,28 +15,25 @@ import (
 // Defaults to binary
 var defaultCodec = binary.Codec
 
-type (
+// Logger is a generic logger interface
+type Logger interface {
+	Fatal(string, ...interface{})
+	Error(string, ...interface{})
+	Warn(string, ...interface{})
+	Info(string, ...interface{})
+	Debug(string, ...interface{})
+	Trace(string, ...interface{})
+}
 
-	// Logger is a generic logger interface
-	Logger interface {
-		Fatal(string, ...interface{})
-		Error(string, ...interface{})
-		Warn(string, ...interface{})
-		Info(string, ...interface{})
-		Debug(string, ...interface{})
-		Trace(string, ...interface{})
-	}
-
-	// Driver is what is used to interact with the scribble database. It runs
-	// transactions, and provides log output
-	Driver struct {
-		codec   codec.MarshalUnmarshaler
-		mutex   sync.Mutex
-		mutexes map[string]*sync.Mutex
-		dir     string // the directory where scribble will create the database
-		log     Logger // the logger scribble will log to
-	}
-)
+// Driver is what is used to interact with the scribble database. It runs
+// transactions, and provides log output
+type Driver struct {
+	codec   codec.MarshalUnmarshaler
+	mutex   sync.Mutex
+	mutexes map[string]*sync.Mutex
+	dir     string // the directory where scribble will create the database
+	log     Logger // the logger scribble will log to
+}
 
 // Options uses for specification of working golang-scribble
 type Options struct {
